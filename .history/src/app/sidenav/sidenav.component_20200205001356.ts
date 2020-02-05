88941fc0f45b8da160002285fe0ea4a14afc8458
Tejeshwar <tejeshwarkumar321@gmail.com>
2020-02-05 18:41:21 +0530

    mobile responsive done

---
+++ .history/src/app/sidenav/sidenav.component_20200205001356.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ChangeDetectorRef,} from '@angular/core';
+import { faFacebook, faTwitter, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
+
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent implements OnInit {
+
+  mobileQuery: MediaQueryList;
+  faFacebook = faFacebook;
+  faTwitter = faTwitter;
+  faInsta = faInstagram;
+  faYT = faYoutube;
+
+  // fillerNav = Array(5).fill(0).map((_, i) => `Nav Item ${i + 1}`);
+  fillerNav = ['home','events','mega-events','intra','team','sponsors','highlights','about']
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+
+  ngOnInit() {
+  }
+
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  shouldRun = true;
+
+}
